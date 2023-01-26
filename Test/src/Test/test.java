e35b9161102a845a0f32b4fefc1d25e56c663dbf
wjy0904 <wjy0904@hanmail.net>
2023-01-26 21:19:16 +0900

    first commit test

---
+++ Test/src/Test/test.java
@@ -0,0 +1,12 @@
+package Test;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This");
+		System.out.println("is");
+		System.out.println("Test");
+	}
+
+}
